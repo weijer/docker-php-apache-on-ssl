fe74ec619948e1c3695b5af257eda9875aa7de24
weijer <chengwei5566>
2019-10-12 12:14:19 +0800

    add:增加php扩展

--- 7/7.2/entrypoint.sh
+++ 7/7.2/entrypoint.sh
@@ -5,5 +5,5 @@ servername=$2
 
 mkdir -p ${path}
 openssl genrsa -out ${path}/server.key 2048
-openssl req -new -key ${path}/server.key -out ${path}/server.csr -subj '/C=JP/ST=Tokyo/L=Tokyo/O=Example Ltd./OU=Web/CN='${servername}
+openssl req -new -key ${path}/server.key -out ${path}/server.csr -subj '/C=CN/ST=BeiJing/L=BeiJing/O=CloudDisk/OU=Web/FE='${servername}
 openssl x509 -in ${path}/server.csr -days 3650 -req -signkey ${path}/server.key -out ${path}/server.crt
