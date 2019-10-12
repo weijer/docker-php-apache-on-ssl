49b9d9aec10237629c40e0a2ddeb416cd122b884
weijer <chengwei5566>
2019-10-12 15:15:29 +0800

    add:自定义openssl.cnf

--- 7/7.2/entrypoint.sh
+++ 7/7.2/entrypoint.sh
@@ -6,4 +6,4 @@ servername=$2
 mkdir -p ${path}
 openssl genrsa -out ${path}/server.key 2048
 openssl req -new -key ${path}/server.key -out ${path}/server.csr -subj '/C=CN/ST=BeiJing/L=BeiJing/O=CloudDisk/OU=Web/FE='${servername}
-openssl x509 -in ${path}/server.csr -days 3650 -req -signkey ${path}/server.key -out ${path}/server.crt
+openssl x509 -in ${path}/server.csr -days 3650 -req -signkey ${path}/server.key -out ${path}/server.crt -config /usr/lib/ssl/openssl.cnf -extensions v3_req
